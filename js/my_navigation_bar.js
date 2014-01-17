0072fbce446815c77ead9d4a4c7f665123d685e0
Sahil Jain <sahil.jain@live.ca>
2014-01-17 01:23:34 -0500

    Speeded up load-time by replacing Javascript lines with hardcoded html

--- js/my_navigation_bar.js
+++ js/my_navigation_bar.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$("nav").load("/my_nav_bar.html");
-});
+});*/
+$(document).ready(function() {
+
+                var num = parseInt($("nav").attr("page"))+1;
+               $(".navbar-left > li:nth-child(" + num + ")").addClass('active');
+
+              });
